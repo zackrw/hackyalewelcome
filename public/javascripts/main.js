4c6382431443e016bc2da08758ba97503cbc4210
Zack Reneau-Wedeen <xaq2892@yahoo.com>
2012-06-03 21:39:15 -0400

    Make double raining impossible

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,9 +1,15 @@
+var raining = false;
+
 $(function(){
   $('#rain').click(function(){
-    $(document).snowfall();
-    $('#confetti_0').snowfall({flakeCount : 150, maxSpeed : 8, flakeColor : "#F09", round: false, shadow:true, minSize:3, maxSize:7});
-    $('#confetti_1').snowfall({flakeCount : 150, maxSpeed : 8, flakeColor : "#6F0", round: false, shadow:true, minSize:3, maxSize:7});
-    $('#confetti_2').snowfall({flakeCount : 150, maxSpeed : 8, flakeColor : "#09F", round: false, shadow:true, minSize:3, maxSize:7});
-    $('#confetti_3').snowfall({flakeCount : 150, maxSpeed : 8, flakeColor : "#FF9", round: false, shadow:true, minSize:3, maxSize:7});  
+    if(!raining){
+      $(document).snowfall();
+      $('#confetti_0').snowfall({flakeCount : 150, maxSpeed : 8, flakeColor : "#F09", round: false, shadow:true, minSize:3, maxSize:7});
+      $('#confetti_1').snowfall({flakeCount : 150, maxSpeed : 8, flakeColor : "#6F0", round: false, shadow:true, minSize:3, maxSize:7});
+      $('#confetti_2').snowfall({flakeCount : 150, maxSpeed : 8, flakeColor : "#09F", round: false, shadow:true, minSize:3, maxSize:7});
+      $('#confetti_3').snowfall({flakeCount : 150, maxSpeed : 8, flakeColor : "#FF9", round: false, shadow:true, minSize:3, maxSize:7});  
+      
+      raining = true;
+    }
   });
 });
